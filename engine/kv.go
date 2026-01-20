aa60aa9e38151e42b3b60b1c406f2f1dd8f9c2c7
Aetherance <inkToAether@gmail.com>
2026-01-20 23:49:26 +0800

    feat: implement kv.go for single machine
    Signed-off-by: Aetherance <inkToAether@gmail.com>

--- engine/kv.go
+++ engine/kv.go
@@ -1,12 +1,51 @@
 package engine
 
-import "github.com/Aetherance/kv/storage"
+import (
+	"github.com/Aetherance/kv/common"
+	"github.com/Aetherance/kv/storage"
+)
 
 type KV struct {
-	storage storage.StorageEngine
-	tso     *TimestampOracle
+	storage storage.Storage
+	tso     *Tso
 }
 
-func (kv *KV) Get(key []byte) ([]byte, error)
-func (kv *KV) Set(key, val []byte) error
-func (kv *KV) BeginTxn() Transaction
+func (kv *KV) Get(key []byte) ([]byte, error) {
+	if len(key) == 0 {
+		return nil, common.ErrEngineEmptyKey
+	}
+
+	commitTs := kv.tso.GetNextTs()
+	val, ok := kv.storage.Get(string(key), commitTs)
+
+	if !ok {
+		return nil, common.ErrEngineKeyNotFound
+	} else {
+		return val, nil
+	}
+}
+
+func (kv *KV) Set(key, val []byte) error {
+	if len(key) == 0 {
+		return common.ErrEngineEmptyKey
+	}
+
+	commitTs := kv.tso.GetNextTs()
+	kv.storage.Set(string(key), val, commitTs)
+
+	return nil
+}
+
+func (kv *KV) Del(key []byte) error {
+	if len(key) == 0 {
+		return common.ErrEngineEmptyKey
+	}
+
+	commitTs := kv.tso.GetNextTs()
+	kv.storage.Delete(string(key), commitTs)
+	return nil
+}
+
+func (kv *KV) BeginTxn() Transaction {
+	return nil
+}
