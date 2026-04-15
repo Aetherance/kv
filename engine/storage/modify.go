c829280ad27971307d461d0ce58e88a1202bb5d4
Aetherance <inkToAether@gmail.com>
2026-04-16 01:46:57 +0800

    feat: introduce Storage for kv engine
    Signed-off-by: Aetherance <inkToAether@gmail.com>

--- engine/storage/modify.go
+++ engine/storage/modify.go
@@ -4,3 +4,44 @@ package storage
 type Modify struct {
 	Data interface{}
 }
+
+type Put struct {
+	Cf  string
+	Key []byte
+	Val []byte
+}
+
+type Delete struct {
+	Cf  string
+	Key []byte
+}
+
+func (m *Modify) Key() []byte {
+	switch data := m.Data.(type) {
+	case Put:
+		return data.Key
+	case Delete:
+		return data.Key
+	default:
+		return nil
+	}
+}
+
+func (m *Modify) Value() []byte {
+	if data, ok := m.Data.(Put); ok {
+		return data.Val
+	}
+
+	return nil
+}
+
+func (m *Modify) Cf() string {
+	switch data := m.Data.(type) {
+	case Put:
+		return data.Cf
+	case Delete:
+		return data.Cf
+	default:
+		return ""
+	}
+}
