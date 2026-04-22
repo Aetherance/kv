9cd4c474ee2e16efe65878838f80bf30658ec9aa
Aetherance <inkToAether@gmail.com>
2026-04-22 15:18:34 +0800

    chore: extend the reader interface

--- engine/storage/storage.go
+++ engine/storage/storage.go
@@ -1,10 +1,21 @@
 package storage
 
-import "github.com/Aetherance/kv/proto/pkg/kvrpcpb"
+import (
+	util "github.com/Aetherance/kv/engine/util"
+	"github.com/Aetherance/kv/proto/pkg/kvrpcpb"
+)
 
 type Storage interface {
 	Start() error
 	Stop() error
 	Write(ctx *kvrpcpb.Context, batch []Modify) error
-	Read(ctx *kvrpcpb.Context) error
+	Reader(ctx *kvrpcpb.Context) (StorageReader, error)
+}
+
+// StorageReader is the minimal read-side abstraction for storage backends.
+// Concrete read methods will be added with the raw KV API.
+type StorageReader interface {
+	GetCF(cf string, key []byte) ([]byte, error)
+	IterCF(cf string) util.Iterator
+	Close()
 }
