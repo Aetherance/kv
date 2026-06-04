ed66cb1658b75b237d30780ee9dabc5b1bc3cc53
Aetherance <inkToAether@gmail.com>
2026-06-04 14:40:35 +0800

    log: add tiered logging

---
+++ log/log.go
@@ -0,0 +1,12 @@
+package log
+
+// Stub logging helpers. They intentionally do nothing for now; replace with a
+// real logger implementation later.
+
+func Debugf(format string, args ...interface{}) {}
+func Infof(format string, args ...interface{})  {}
+func Warnf(format string, args ...interface{})  {}
+func Errorf(format string, args ...interface{}) {}
+func Fatalf(format string, args ...interface{}) {}
+func Fatal(args ...interface{})                 {}
+func Error(args ...interface{})                 {}
