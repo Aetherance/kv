e18daf2717a660acefc45b50a45fe3a993a15622
Aetherance <inkToAether@gmail.com>
2026-08-02 22:39:59 +0800

    storage: remove unused engine scaffolding

--- engine/util/write_batch.go
+++ engine/util/write_batch.go
@@ -6,11 +6,7 @@ import (
 )
 
 type WriteBatch struct {
-	entries       []*badger.Entry
-	size          int
-	safePoint     int
-	safePointSize int
-	safePointUndo int
+	entries []*badger.Entry
 }
 
 const (
@@ -30,21 +26,18 @@ func (wb *WriteBatch) SetCF(cf string, key, val []byte) {
 		Key:   KeyWithCF(cf, key),
 		Value: val,
 	})
-	wb.size += len(key) + len(val)
 }
 
 func (wb *WriteBatch) DeleteMeta(key []byte) {
 	wb.entries = append(wb.entries, &badger.Entry{
 		Key: key,
 	})
-	wb.size += len(key)
 }
 
 func (wb *WriteBatch) DeleteCF(cf string, key []byte) {
 	wb.entries = append(wb.entries, &badger.Entry{
 		Key: KeyWithCF(cf, key),
 	})
-	wb.size += len(key)
 }
 
 func (wb *WriteBatch) SetMeta(key []byte, msg proto.Message) error {
@@ -56,18 +49,7 @@ func (wb *WriteBatch) SetMeta(key []byte, msg proto.Message) error {
 		Key:   key,
 		Value: val,
 	})
-	wb.size += len(key) + len(val)
 	return nil
-}
-
-func (wb *WriteBatch) SetSafePoint() {
-	wb.safePoint = len(wb.entries)
-	wb.safePointSize = wb.size
-}
-
-func (wb *WriteBatch) RollbackToSafePoint() {
-	wb.entries = wb.entries[:wb.safePoint]
-	wb.size = wb.safePointSize
 }
 
 func (wb *WriteBatch) WriteToDB(db *badger.DB) error {
@@ -91,19 +73,4 @@ func (wb *WriteBatch) WriteToDB(db *badger.DB) error {
 		}
 	}
 	return nil
-}
-
-func (wb *WriteBatch) MustWriteToDB(db *badger.DB) {
-	err := wb.WriteToDB(db)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (wb *WriteBatch) Reset() {
-	wb.entries = wb.entries[:0]
-	wb.size = 0
-	wb.safePoint = 0
-	wb.safePointSize = 0
-	wb.safePointUndo = 0
 }
